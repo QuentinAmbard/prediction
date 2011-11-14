f7adc6bde048a9fb08faae01657a7070fcf78afa
mmouminoux <matthieu.mouminoux@gmail.com>
2011-11-14 23:01:33 +0100

    add twitter stream lib

--- prediction-data/src/main/java/com/avricot/prediction/TwitterListener.java
+++ prediction-data/src/main/java/com/avricot/prediction/TwitterListener.java
@@ -1,10 +1,48 @@
 package com.avricot.prediction;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
+
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+import com.avricot.prediction.repository.tweet.TweetRepository;
 
 @Service
 public class TwitterListener {
-	public void listen(){
-		
+	@Inject
+	TweetRepository tweeterRepository;
+
+	public void listen() {
+		// tweeterRepository.save(new Tweet());
+
+		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+		StatusListener listener = new StatusListener() {
+			public void onStatus(Status status) {
+				System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+			}
+
+			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+				System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+			}
+
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+				System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+			}
+
+			public void onScrubGeo(long userId, long upToStatusId) {
+				System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+			}
+
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+		};
+		twitterStream.addListener(listener);
+		twitterStream.sample();
 	}
 }
