9f1c41168c878d4cb335b211398cde5dc4f3c947
qambard <quentin.ambard@gmail.com>
2011-12-06 21:42:55 +0100

    mashup

--- prediction-mashup/src/main/java/com/avricot/prediction/MashupBuzz.java
+++ prediction-mashup/src/main/java/com/avricot/prediction/MashupBuzz.java
@@ -26,10 +26,10 @@ public class MashupBuzz {
 	private TweetRepository tweetRepository;
 	@Inject
 	private CandidatRespository candidatRepository;
-	
-	static final long MILLIS_IN_A_DAY = 1000*60*60*24;
+
+	static final long MILLIS_IN_A_DAY = 1000 * 60 * 60 * 24;
 	private static Logger LOG = Logger.getLogger(MashupBuzz.class);
-	
+
 	/**
 	 * Mashup all the days.
 	 */
@@ -39,7 +39,7 @@ public class MashupBuzz {
 			mashup(report.getTimestamp(), maxTweet, maxRss, maxInsight);
 		}
 	}
-	
+
 	/**
 	 * Mashup the data for the given date.
 	 * 
@@ -55,7 +55,7 @@ public class MashupBuzz {
 	public void mashupDailyBuzz(long maxTweet, long maxRss, float maxInsight) {
 		mashup(new Date(System.currentTimeMillis()), maxTweet, maxRss, maxInsight);
 	}
-	
+
 	/**
 	 * Mashup the data for the given date.
 	 * 
@@ -71,70 +71,84 @@ public class MashupBuzz {
 		long totalTweet = 0;
 		long totalRss = 0;
 		float insightScore;
-		
+
 		for (Candidat candidat : candidats) {
 			CandidatReport dailyReport = report.getCandidats().get(candidat.getCandidatName());
 			long todayTweets = tweetRepository.count(candidat.getCandidatName(), startDate, endDate);
 			tweetCountMap.put(candidat.getCandidatName(), todayTweets);
 			totalTweet += todayTweets;
-			
+
 			long todayRSS = 0;
-			if(dailyReport != null)
-				todayRSS = (long) dailyReport.getRssCountResult();
-			
-		 	rssCountMap.put(candidat.getCandidatName(), todayRSS);
-		 	totalRss += todayRSS;
+			if (dailyReport != null)
+				todayRSS = dailyReport.getRssCountResult();
+
+			rssCountMap.put(candidat.getCandidatName(), todayRSS);
+			totalRss += todayRSS;
 		}
-		
-		/* Calcul du tweetscore = nombre de tweets pour un candidat / tous les tweets de la journée parlant des candidats */
+
+		/*
+		 * Calcul du tweetscore = nombre de tweets pour un candidat / tous les
+		 * tweets de la journée parlant des candidats
+		 */
 		for (CandidatName key : tweetCountMap.keySet()) {
 			CandidatReport dailyReport = report.getCandidats().get(key);
-			if(dailyReport.getCandidatName() == null)
+			if (dailyReport.getCandidatName() == null)
 				dailyReport.setCandidatName(key);
-				
-			if(totalTweet != 0)
-				dailyReport.setTweetScore((tweetCountMap.get(key) / totalTweet) * 100); 
-			
-			if(totalRss != 0)
-				dailyReport.setRssScore((rssCountMap.get(key) / totalRss) * 100);
-			else
+
+			if (totalTweet != 0) {
+				long tweetScore = (tweetCountMap.get(key) / totalTweet) * 100;
+				maxTweet = Math.max(tweetScore, maxTweet);
+				dailyReport.setTweetScore(tweetScore);
+			}
+
+			if (totalRss != 0) {
+				long rssScore = (rssCountMap.get(key) / totalRss) * 100;
+				maxRss = Math.max(rssScore, maxRss);
+				dailyReport.setRssScore(rssScore);
+			} else {
 				dailyReport.setRssScore(0);
-			
+			}
+
 			insightScore = (dailyReport.getInsight() / maxInsight) * 100;
-			
-			if(dailyReport.getRssScore() == 0)
-				dailyReport.setBuzz(dailyReport.getTweetScore() + insightScore / 2);
-			else
-				dailyReport.setBuzz(dailyReport.getRssScore() + dailyReport.getTweetScore() + insightScore / 3);
-			
+
+			if (dailyReport.getRssScore() == 0) {
+				dailyReport.setBuzz((dailyReport.getTweetScore() * 100 / maxTweet + insightScore * 100 / maxRss) / 2);
+			} else {
+				dailyReport.setBuzz((dailyReport.getRssScore() * 100 / maxTweet + dailyReport.getTweetScore() * 100 / maxRss + insightScore) / 3);
+			}
+
 			LOG.info("BUZZ pour " + key.toString() + " - " + dailyReport.getBuzz());
-			/* Le désintéressement est l'inverse de la polarité négative * la popularité */
-			if(dailyReport.getNeg() != 0)
-				dailyReport.setNone((1/dailyReport.getNeg()) * dailyReport.getBuzz());
+			/*
+			 * Le désintéressement est l'inverse de la polarité négative * la
+			 * popularité
+			 */
+			if (dailyReport.getNeg() != 0)
+				dailyReport.setNone((1 / dailyReport.getNeg()) * dailyReport.getBuzz());
 		}
-		
+
+		for (CandidatName key : tweetCountMap.keySet()) {
+		}
+
 		/* Calcul de la tendance */
 		Report yesterdayReport = reportRepository.findByTimestamp(midnight - MILLIS_IN_A_DAY);
 		Report dayBeforeYesterdayReport = reportRepository.findByTimestamp(midnight - (MILLIS_IN_A_DAY * 2));
 		float buzzBeforeYesterday;
 		float buzzYesterday;
 		float buzzToday;
-		
-		if(yesterdayReport != null && dayBeforeYesterdayReport != null) {
+
+		if (yesterdayReport != null && dayBeforeYesterdayReport != null) {
 			for (CandidatName key : yesterdayReport.getCandidats().keySet()) {
 				buzzYesterday = yesterdayReport.getCandidats().get(key).getBuzz();
 				buzzBeforeYesterday = dayBeforeYesterdayReport.getCandidats().get(key).getBuzz();
 				buzzToday = report.getCandidats().get(key).getBuzz();
-	
+
 				/* C'est la moyenne des changements sur 3 jours */
 				report.getCandidats().get(key).setTendance((Math.abs(buzzYesterday - buzzBeforeYesterday) + Math.abs(buzzToday - buzzYesterday)) / 2);
 			}
 		} else {
 			LOG.error("Impossible de calculer la tendance.");
 		}
-		
-		
-		
+
 		reportRepository.save(report);
 	}
 }
